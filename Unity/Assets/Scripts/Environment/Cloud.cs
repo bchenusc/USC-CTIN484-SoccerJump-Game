5a07b45c588086f98accd9656d5f07ab78f04a95
Andrew Lee <lee533@usc.edu>
2014-10-05 20:01:04 -0700

    added cloud movement and spawning

--- Unity/Assets/Scripts/Environment/Cloud.cs
+++ Unity/Assets/Scripts/Environment/Cloud.cs
@@ -2,12 +2,17 @@
 
 public class Cloud : MonoBehaviour {
 
-	float mSpeed = 0;
+	float mSpeed;
 
 	public float Speed { get { return mSpeed; } set { mSpeed = value; } }
+
+	void Start () {
+		mSpeed = Random.Range (2, 10) / 100f;
+	}
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.right * mSpeed);
+		if (transform.position.x > 50) Destroy (this.gameObject);
 	}
 }

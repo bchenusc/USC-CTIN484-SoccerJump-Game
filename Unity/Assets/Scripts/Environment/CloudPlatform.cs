1ac0d1795f0f5c75f92df1fee5e0a91311f9abf7
Mr Fluffy Sheep <brianych@usc.edu>
2014-11-11 00:31:26 -0800

    playtested. second ball may be broken.

---
+++ Unity/Assets/Scripts/Environment/CloudPlatform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudPlatform : MonoBehaviour {
+
+	public int direction = 0;
+	bool destroying = false;
+
+	void Update()
+	{
+		if (destroying)
+						return;
+		if (Mathf.Abs(transform.position.x) > 30)
+		{
+			destroying = true;
+			Destroy (gameObject);
+
+		}
+
+		if (direction == 0)
+						direction = 1;
+		direction = direction / Mathf.Abs (direction);
+		transform.Translate (Time.deltaTime * Vector3.right * direction * Random.Range (3, 10) / 3.0f);
+	}
+}
---
+++ Unity/Assets/Scripts/Environment/CloudPlatformSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudPlatformSpawner : MonoBehaviour {
+
+	uint cloudTracker = 0;
+	float minTime = 20.0f;
+	float maxTime = 60.0f;
+	
+	public Transform[] cloudPrefabs = null;
+	
+	// Use this for initialization
+	void Start () {
+		SpawnCloudPlatform ();
+		SingletonObject.Get.getTimer ().Add (gameObject.GetInstanceID () + cloudTracker + "", SpawnCloudPlatform
+		                                     , Random.Range (minTime, maxTime), false);
+		
+	}
+	
+	void SpawnCloudPlatform()
+	{
+		++cloudTracker;
+		// Actually spawn a platform
+		Vector3 spawnPosition = Vector3.zero;
+		int direction = (Random.Range (0, 2) * 2 - 1);
+		spawnPosition.Set (25 * direction, Random.Range (0, 12), 0);
+		Transform clone = Instantiate(cloudPrefabs[Random.Range (0, cloudPrefabs.Length)],
+		                                           spawnPosition, Quaternion.identity) as Transform;
+		clone.GetComponent<CloudPlatform> ().direction = -direction;
+		
+		SingletonObject.Get.getTimer ().Add (gameObject.GetInstanceID () + cloudTracker+"", SpawnCloudPlatform
+		                                     , Random.Range (minTime, maxTime), false);
+	}
+
+}
